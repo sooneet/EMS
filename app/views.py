fb31a7858fd03d06b5e5148210c4ea96dbea39fd
mesooneet <mesooneet@gmail.com>
2022-08-06 20:59:12 +0545

    home view

--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from . models import Employee
+
 
 # Create your views here.
+def home(request):
+    total_employee = Employee.objects.all().count()
+    on_leave = Employee.objects.filter(on_leave=True).count()
+    context = {
+        'total_employee':total_employee,
+        'on_leave':on_leave
+    }
+    return render(request,'dashboard.html',context)
